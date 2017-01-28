f6b1ef50080b3591e376c0b9e604c7fbf086e9ec
Colin Marquardt <cmarqu42@gmail.com>
2017-01-28 23:57:37 +0100

    Add setup.py for setuptools.

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='pytipr',
+    description='The Python Template In-Place Replacer',
+    version="0.01",
+    author='The pytipr Developers',
+    author_email='cmarqu42@gmail.com',
+    url='https://github.com/pytipr/pytipr',
+    packages=['pytipr'],
+    install_requires=['pytest'],
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Developers',
+        'Operating System :: POSIX',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: MacOS :: MacOS X',
+        'Topic :: Utilities',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+    ],
+)
