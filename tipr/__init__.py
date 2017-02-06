b668ed4481a9627b3c4dbb9e09a9dce15d436dc5
Colin Marquardt <cmarqu42@gmail.com>
2017-02-07 00:15:07 +0100

    Do case-insensitive magic string matching.

--- tipr/__init__.py
+++ tipr/__init__.py
@@ -2,9 +2,12 @@
 
 '''pytipr, the Python Template In-Place Replacer.'''
 
+# FIXME: __init__.py should be empty and this code in another file
+
 from __future__ import print_function
 
 import logging
+
 
 def write_file(filename, text='Hello world!'):
     '''Write a simple file.'''
@@ -21,21 +24,21 @@ def do(infilename, outfilename):
     namespace = {}
     with open(infilename, 'r') as rfobj, open(outfilename, 'w') as wfobj:
         for line in rfobj:
-            if line.startswith('## tipr_'):  # hey, something interesting
-                if line.startswith('## tipr_result_is'):
+            # FIXME: we need to keep track of parsing state here
+            if line.lower().startswith('## tipr_'):  # hey, something interesting
+                if line.lower().startswith('## tipr_result_is'):
                     wfobj.write(line)  # repeat in output
                     code = line[len('## '):]
-                    #print('code={0}'.format(code))
-                    exec(code, namespace)  # needed in Python 3
-                    tipr_result_is = namespace['tipr_result_is']
-                    #print('tipr_result_is={0}'.format(tipr_result_is))
-                if line.startswith('## tipr_result_start'):
+                    exec(code, namespace)  # namespace needed in Python 3
+                    namespace_lower = {k.lower():v for k,v in namespace.items()}
+                    tipr_result_is = namespace_lower['tipr_result_is']
+                if line.lower().startswith('## tipr_result_start'):
                     wfobj.write(line)  # repeat in output
                     wfobj.write(tipr_result_is+'\n')  # write the result
-                if line.startswith('## tipr_result_end'):
+                if line.lower().startswith('## tipr_result_end'):
                     wfobj.write(line)  # repeat in output
                 # tipr_code_start and tipr_code_end:
-                if line.startswith('## tipr_code'):
+                if line.lower().startswith('## tipr_code'):
                     wfobj.write(line)  # repeat in output
             else:
                 wfobj.write(line)  # nothing interesting, pass through
